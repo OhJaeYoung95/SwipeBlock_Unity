b31b75ee10845bc6284f85c693889d5e8bf966a2
오재영 <ostyoung@naver.com>
2023-10-18 18:02:15 +0900

    다음 작업 : 사운드 매니저

--- Assets/Scripts/Sonund/SoundManager.cs
+++ Assets/Scripts/Sonund/SoundManager.cs
@@ -2,13 +2,58 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public static class SoundManager
+public class SoundManager : MonoBehaviour
 {
-    public static AudioClip AudioClip { get; private set; }
+    public static SoundManager Instance { get; private set; }
+    public AudioSource[] effectsAudio;
+    public AudioSource musicAudio;
 
-
-    public static void Init()
+    private void Awake()
     {
-        Resources.Load<AudioClip>("");
+        Instance = this;
+        DontDestroyOnLoad(gameObject);
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void SetEffectsVolume(float volume)
+    {
+        //AudioSource[] bunnyAudios = bunnys.GetComponentsInChildren<AudioSource>();
+        //AudioSource[] bearAudios = bears.GetComponentsInChildren<AudioSource>();
+        //AudioSource[] elephantAudios = elephants.GetComponentsInChildren<AudioSource>();
+
+        //foreach (var bunnyAudio in bunnyAudios)
+        //{
+        //    bunnyAudio.volume = volume;
+        //}
+
+        //foreach (var bearAudio in bearAudios)
+        //{
+        //    bearAudio.volume = volume;
+        //}
+
+        //foreach (var elephantAudio in elephantAudios)
+        //{
+        //    elephantAudio.volume = volume;
+        //}
+
+        //foreach (var effect in effectsAudio)
+        //{
+        //    effect.volume = volume;
+        //}
+    }
+    public void SetMusicVolume(float volume)
+    {
+        musicAudio.volume = volume;
     }
 }
